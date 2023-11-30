bbffaf240022b7ebf10f64ef74e2875c2a607425
gsrsedan87 <100337263+gsrsedan87@users.noreply.github.com>
2023-11-30 09:39:39 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('quoteForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Quote request submitted! We will get back to you soon.');
+});
